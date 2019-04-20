172782be5fc3e262da567a336b7a288a198df555
K Shah <kshah707@gmail.com>
2019-04-20 19:03:21 -0400

    Winding number algorithm implemented (signed crossings variant)

--- PolyWind.py
+++ PolyWind.py
@@ -1,4 +1,36 @@
+import numpy as np
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def ccw(a, b, c):
+    return np.sign(np.linalg.det(np.array([[a.x, b.x, c.x],
+                                          [a.y, b.y, c.y],
+                                          [1, 1, 1]])))
+
+def windingNum(point, polygon):
+    n = len(polygon)
+    crossings = 0
+    
+    for i in range(0, n):
+        #is a crossing with edge pointing upward
+        if (polygon[i]).y <= point.y and (polygon[i+1]).y > point.y:
+            #point is to the left of the edge
+            if ccw(polygon[i], polygon[i+1], point) == 1:
+                crossings += 1
+        #is a crossing with edge pointing downward
+        if (polygon[i]).y > point.y and (polygon[i+1]).y <= point.y:
+            #point is to the right of the edge
+            if ccw(polygon[i], polygon[i+1], point) == -1:
+                crossings -= 1
+    return crossings
+
+###################
+
 from graphics import *
+
 def main():
     win = GraphWin("My Circle", 100, 100)
     c = Circle(Point(50,50), 10)
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # PolyWind
 Demonstrates and computes winding number for a point in a non-simple polygon.
 Contributors: Radu Visina, Taylore Westbrook, and Keyur Shah.
-Based on the journal chapter: Eric Haines, "Point in Polygon Strategies" in Graphics Gems IV (1994)
+References consulted:
+Eric Haines, "Point in Polygon Strategies" chapter of Graphics Gems IV (1994)
+Dan Sunday, "Inclusion of a Point in a Polygon" webpage at GeomAlgorithms.com
