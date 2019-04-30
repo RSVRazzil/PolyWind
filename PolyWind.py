6efc222e6243a32807ec7d297e6b081f2e8f4504
Taylore <taylorejwestbrook@gmail.com>
2019-04-30 02:38:35 -0400

    add directional arrows to polygon edges, label ray intersections with crossing number, merged with ability to set winding point

--- PolyWind.py
+++ PolyWind.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#Some constants
 BUTTON_X = 80
 BUTTON_Y = 40
 WIDTH = 1024
@@ -9,12 +10,6 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-#class Polygon:
-#    def __init__(self):
-#        self.pointList = list()
-        
-#    def 
 
 def ccw(a, b, c):
     return np.sign(np.linalg.det(np.array([[a.x, b.x, c.x],
@@ -38,7 +33,7 @@ def windingNum(point, polygon):
             #point is to the right of the edge
             if ccw(polygon[i], polygon[(i+1) % n], point) == -1:
                 crossings -= 1
-    return np.abs(crossings)
+    return crossings
 
 def intersect(lineA, lineB): #intersection point of two lines
     xdiff = (lineA.getP1().x - lineA.getP2().x, lineB.getP1().x - lineB.getP2().x)
@@ -116,62 +111,64 @@ def main():
     stopLoop = False
     win = GraphWin("My Circle", WIDTH, HEIGHT)
     c = Circle(Point(50,50), 10)
-    pointList = []
+    polygonPointList = []
     #Construct "Clear" button. Draw it in loop.
     clearButton = Button("Clear", 900, 10) 
     win.setBackground("black")
     #windingPoint is the point around which the polygon winds
-    windingPoint = Point(WIDTH/2, HEIGHT/2)
+    windingPoint = None
     #initialize winding number
     windingNumValue = 0
-
+    
     while stopLoop == False:    #The main graphics/input processing loop
         #Clear the drawing area
         win.delete("all")
-        #initialize ray to count edge crossings from winding point
-        ray = Line(windingPoint, Point(WIDTH/2, HEIGHT))
-        ray.setWidth(3)
-        ray.setFill("red")
-        ray.setOutline("red")
-        ray.draw(win)
         #initialize crossing number
         crossingNumValue = 0
-        #Draw the lines representing the polygon
-        for pointIndex in range(len(pointList)):
-            if pointIndex == len(pointList) - 1:
-                #When reaching last point in polygon list, connect with first point
-                line = Line(pointList[pointIndex], pointList[0])
-            else:
-                #For all other points, connect line from current point to next point
-                line = Line(pointList[pointIndex], pointList[pointIndex + 1])
-            #Set up remaining line attiorbutes and draw it
-            line.setWidth(3)
-            line.setFill("lightgreen")
-            line.setOutline("lightgreen")
-            line.setArrow("last")
-            line.draw(win)
-            #find intersection of ray and edge
-            intersection = intersect(line, ray)
-            #display number of crossings from winding point to edge
-            if intersection != 0:
-                crossingNumValue = crossingNumValue + 1
-                rayText = Text(Point(intersection.x + 10, intersection.y - 10), str(crossingNumValue))
-                rayText.setOutline("white")
-                rayText.draw(win)
+        if windingPoint != None:
+            #initialize ray to count edge crossings from winding point
+            ray = Line(windingPoint, Point(WIDTH/2, HEIGHT))
+            ray.setWidth(3)
+            ray.setFill("red")
+            ray.setOutline("red")
+            ray.draw(win)
+            #Draw the lines representing the polygon
+            for pointIndex in range(len(polygonPointList)):
+                if pointIndex == len(polygonPointList) - 1:
+                    #When reaching last point in polygon list, connect with first point
+                    line = Line(polygonPointList[pointIndex], polygonPointList[0])
+                else:
+                    #For all other points, connect line from current point to next point
+                    line = Line(polygonPointList[pointIndex], polygonPointList[pointIndex + 1])
+                #Set up remaining line attiorbutes and draw it
+                line.setWidth(3)
+                line.setFill("lightgreen")
+                line.setOutline("lightgreen")
+                line.setArrow("last")
+                line.draw(win)
+
+                #find intersection of ray and edge
+                intersection = intersect(line, ray)
+                #display number of crossings from winding point to edge
+                if intersection != 0:
+                    crossingNumValue = crossingNumValue + 1
+                    rayText = Text(Point(intersection.x + 10, intersection.y - 10), str(crossingNumValue))
+                    rayText.setOutline("white")
+                    rayText.draw(win)
             
-        #Draw a circle to represent the winding point
-        pointCircle = Circle(windingPoint,3)
-        pointCircle.setFill("white")
-        pointCircle.setOutline("white")
-        pointCircle.draw(win)
-        
-        windingNumValue = windingNum(windingPoint, pointList)
-        
-        #Draw text representing winding number, offset from circle center
-        pointText = Text(Point(windingPoint.x + 15, windingPoint.y - 10), str(windingNumValue))
-        pointText.setOutline("white")
-        pointText.draw(win)
-        clearButton.Draw(win)
+                #Draw a circle to represent the winding point
+                pointCircle = Circle(windingPoint,3)
+                pointCircle.setFill("white")
+                pointCircle.setOutline("white")
+                pointCircle.draw(win)
+                
+                windingNumValue = windingNum(windingPoint, polygonPointList)
+                
+                #Draw text representing winding number, offset from circle center
+                pointText = Text(Point(windingPoint.x + 15, windingPoint.y - 10), str(windingNumValue))
+                pointText.setOutline("white")
+                pointText.draw(win)
+            clearButton.Draw(win)
         
         
         #The check functions below do not stop to wait for input, but we only
@@ -188,13 +185,16 @@ def main():
                 #Have the clear button check whether it was pressed given the click point
                 clearButton.TestPoint(mousePressed)
                 if clearButton.Pressed():
-                    pointList.clear()
+                    windingPoint = None
+                    polygonPointList.clear()
                     #Tell button to reset pressed state
                     clearButton.AckPress()
+                elif windingPoint == None:
+                    windingPoint = mousePressed
                 else:
                     #Add a point to the polygon point list!
-                    pointList.append(mousePressed)
-                    print(pointList)
+                    polygonPointList.append(mousePressed)
+                    print(polygonPointList)
             
             if keyPressed != "":
                 somethingPressed = True
